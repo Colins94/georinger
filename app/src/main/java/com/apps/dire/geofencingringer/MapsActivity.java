85d18667d79ffb8300a02bea30a33fc9d5d06a6f
Colin <snakewind07@gmail.com>
2017-02-08 14:49:56 +0000

    To do- maps activity

---
+++ app/src/main/java/com/apps/dire/geofencingringer/MapsActivity.java
@@ -0,0 +1,58 @@
+package com.apps.dire.geofencingringer;
+
+import android.*;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(lastLocation()));
+
+    }
+    public LatLng lastLocation() {
+        double lang = getIntent().getDoubleExtra("lng",0);
+        double lat = getIntent().getDoubleExtra("lat",0);
+
+        LatLng current = new LatLng(lat,lang);
+        Log.i("return lang", Double.toString(lang) );
+        Log.i("return latt", Double.toString(lat) );
+        return current;
+    }
+
+
+}
